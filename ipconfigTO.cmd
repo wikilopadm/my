0506d31f9366823dd6347363b77f592b3ee6faac
Laurent PERRET <75728070+wikolopadm@users.noreply.github.com>
2022-06-25 15:30:48 +0200

    Update ipconfigTO.cmd

--- ipconfigTO.cmd
+++ ipconfigTO.cmd
@@ -1,8 +1,4 @@
 @cls
 @echo off
-@echo IPV4 :
-@for /f "tokens=2 delims=:" %%a in ('ipconfig^|find "IPv4"') do @set ip=%%a | @echo %%a
-@echo IPV6 :
-@for /f "tokens=2 delims=:" %%a in ('ipconfig^|find "IPv6"') do @set ip=%%a | @echo %%a
-:BREAK
-@pause
+@ipconfig /all >C:\Temp\Logfiles\ipConfigTo_%date%.log
+@"C:\Program Files\Notepad++\notepad++.exe" C:\Temp\Logfiles\ipConfigTo_%date%.log

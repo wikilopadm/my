b29376e74348acf7324ccbe53cd771fb9519563d
Laurent PERRET <75728070+wikolopadm@users.noreply.github.com>
2023-01-19 18:04:29 +0100

    Update ipconfig.cmd

--- ipconfig.cmd
+++ ipconfig.cmd
@@ -1,16 +1,7 @@
+@rem ########################################
+@rem #  IPs - 29 mars 2023                  #
+@rem ########################################
 @cls
 @echo off
-@set X=
-@set HD=
-@for /f "skip=1 delims=" %%x in ('wmic os get localdatetime') do if not defined X set X=%%x
-@set DATE.YEAR=%X:~0,4%
-@set DATE.MONTH=%X:~4,2%
-@set DATE.DAY=%X:~6,2%
-@set DATE.HOUR=%X:~8,2%
-@set DATE.MINUTE=%X:~10,2%
-@set DATE.SECOND=%X:~12,2%
-@set DATE.FRACTIONS=%X:~15,6%
-@set DATE.OFFSET=%X:~21,4%
-@set HD=%DATE.HOUR%-%DATE.MINUTE%-%DATE.SECOND%-%DATE.YEAR%-%DATE.MONTH%-%DATE.DAY%
-@ipconfig/all>IPConfig.txt
-@"C:\Program Files\Notepad++\notepad++.exe" IPConfig.txt
+@ipconfig /all >C:\Temp\Logfiles\ipConfigTo_%date%.log
+@"C:\Program Files\Notepad++\notepad++.exe" C:\Temp\Logfiles\ipConfigTo_%date%.log
